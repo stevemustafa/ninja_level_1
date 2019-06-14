3d73eada295e33269c71d3063679589b302a35dc
Steven Mustafa <steven.i.mustafa@gmail.com>
2019-06-14 18:09:27 +0300

    Completed the exercise
    Amended commit:
    
    Though the code executes correctly, the warning is that we should not be
    specifying the variable type and simply letting the compiler detect what
    the type is.
    
    SM

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import "fmt"
 
-var x int
-var y string
-var z bool
+var x int = 42
+var y string = "James Bond"
+var z bool = true
 
 func main() {
-	fmt.Println("X: ", x)
-	fmt.Println("Y: ", y)
-	fmt.Println("Z: ", z)
+	//fmt.Println("X: ", x)
+	//fmt.Println("Y: ", y)
+	//fmt.Println("Z: ", z)
+	s := fmt.Sprintf("%v\t%v\t%v", x, y, z)
+	fmt.Println(s)
 }
